c8e777803c8d7c7461929fb8c93fac836c7cb72a
highmic <highmic2407@gmail.com>
2020-10-15 18:56:13 -0500

    updated app file

--- app.py
+++ app.py
@@ -1,20 +1,104 @@
 import os
 import sys
 
-from flask import Flask, render_template, redirect, request
-from predict import predict_new
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
-# Create an instance of Flask
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+import re
+import base64
+
+import numpy as np
+
+from PIL import Image
+from io import BytesIO
+
+
+def base64_to_pil(img_base64):
+   
+    image_data = re.sub('^data:image/.+;base64,', '', img_base64)
+    pil_image = Image.open(BytesIO(base64.b64decode(image_data)))
+    return pil_image
+
+
+def np_to_base64(img_np):
+   
+    img = Image.fromarray(img_np.astype('uint8'), 'RGB')
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    return u"data:image/png;base64," + base64.b64encode(buffered.getvalue()).decode("ascii")
+
+
 app = Flask(__name__)
 
-image_new = "images/Dog.jpeg"
+print('Model loaded. Check http://127.0.0.1:5000/')
 
-# print("---------------> FLASK < --------------")
-@app.route("/", methods=["GET", "POST"])
-def home():
-        labels = predict_new(image_new)
-        label = labels[0]
-        return render_template('index.html', message = label)
-if __name__ == "__main__":
-    app.run(debug=True)
 
+# Model saved with Keras
+MODEL_PATH = 'saved_models/keras_cifar10_trained_model.h5'
+
+# Load your own trained model
+model = load_model(MODEL_PATH)
+print('Start serving')
+
+
+def model_predict(img, model):
+    img = img.resize((32, 32))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    x = preprocess_input(x, mode='tf')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+
+        # Make prediction
+        preds = model_predict(img, model)
+        print(preds, "ATTENTION")
+
+        class_names = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+        # find the index of the class with maximum score
+        pred = np.argmax(preds, axis=-1)
+        result = class_names[pred[0]]
+        print(result, pred,'TOKEN' )
+       
+        pred_proba = "{:.3f}".format(np.amax(preds))
+        
+        # Serialize the result, you can add additional fields
+        return jsonify(result=result, probability=pred_proba)
+
+    return None
+
+
+if __name__ == '__main__':
+    
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
