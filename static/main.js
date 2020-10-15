d95924ea232b2432ed7a5e70ae63f6a5dc5e279b
highmic <highmic2407@gmail.com>
2020-10-15 18:59:46 -0500

    updated static file

--- static/main.js
+++ static/main.js
@@ -1,63 +1,133 @@
-function getDataUrl(img) {
-    // Create canvas
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    // Set width and height
-    canvas.width = img.width;
-    canvas.height = img.height;
-    // Draw the image
-    ctx.drawImage(img, 0, 0);
-    return canvas.toDataURL('https://testbucketirina.s3.eu-west-2.amazonaws.com/ml-image/img_cat_real.jpeg');
- }
 
-var selectedFile = document.getElementById("file-upload");
-selectedFile.addEventListener("change", selectedFileHandler, false);
+
+var fileDrag = document.getElementById("file-drag");
+var fileSelect = document.getElementById("file-upload");
+
+// Add event listeners
+
+fileSelect.addEventListener("change", fileSelectHandler, false);
+
+
+function fileSelectHandler(e) {
+  
+  var files = e.target.files || e.dataTransfer.files;
+ 
+  for (var i = 0, f; (f = files[i]); i++) {
+    previewFile(f);
+  }
+}
+
+
 var imagePreview = document.getElementById("image-preview");
 var imageDisplay = document.getElementById("image-display");
 var uploadCaption = document.getElementById("upload-caption");
 var predResult = document.getElementById("pred-result");
 var loader = document.getElementById("loader");
 
-function selectedFileHandler(e) {
-  var files = e.target.files;
-  console.log(files);
-  e.preventDefault();
-  file = files[0];
+//========================================================================
+// Main button events
+//========================================================================
+
+function submitImage() {
+  
+  console.log("submit");
+
+  if (!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
+    window.alert("Please select an image before submit.");
+    return;
+  }
+
+  loader.classList.remove("hidden");
+  imageDisplay.classList.add("loading");
+
+  // call the predict function of the backend
+  predictImage(imageDisplay.src);
+}
+
+function clearImage() {
+  // reset selected files
+  fileSelect.value = "";
+
+  // remove image sources and hide them
+  imagePreview.src = "";
+  imageDisplay.src = "";
+  predResult.innerHTML = "";
+
+  hide(imagePreview);
+  hide(imageDisplay);
+  hide(loader);
+  hide(predResult);
+  show(uploadCaption);
+
+  imageDisplay.classList.remove("loading");
+}
+
+function previewFile(file) {
+ 
   console.log(file.name);
   var fileName = encodeURI(file.name);
-  console.log(fileName);
-  var FR = new FileReader();
-  FR.readAsDataURL(file);
 
-  FR.onloadend = () => {
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
     imagePreview.src = URL.createObjectURL(file);
-    console.log(imagePreview.src);
 
     show(imagePreview);
     hide(uploadCaption);
 
+    // reset
     predResult.innerHTML = "";
     imageDisplay.classList.remove("loading");
 
-    let display = document.getElementById("image-display");
-    display.src = FR.result;
-    show(display);
-    console.log(display.src);
+    displayImage(reader.result, "image-display");
   };
-
-  console.log(FR);
-  imagePreview.src = URL.createObjectURL(file);
-  imagePreview.classList.remove("hidden");
-  console.log(imagePreview.src);
-  console.log(typeof(imagePreview.src));
-  console.log((imagePreview.src).length);
 }
 
+//========================================================================
+// Functions
+//========================================================================
+// FETCH
+function predictImage(image) {
+  fetch("/predict", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(image)
+  })
+    .then(resp => {
+      if (resp.ok)
+        resp.json().then(data => {
+          displayResult(data);
+        });
+    })
+    .catch(err => {
+      console.log("Error occured", err.message);
+      window.alert("Something went wrong.");
+    });
+}
+
+function displayImage(image, id) {
+  // display image on given id <img> element
+  let display = document.getElementById(id);
+  display.src = image;
+  show(display);
+}
+
+function displayResult(data) {
+  // display the result
+
+  hide(loader);
+  predResult.innerHTML = data.result;
+  show(predResult);
+}
 
 function hide(el) {
+  // hide an element
   el.classList.add("hidden");
 }
 
 function show(el) {
+  // show an element
   el.classList.remove("hidden");
 }
